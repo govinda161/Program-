29c9af214d85e356828e26bc4e38db68b321991c
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 16:04:44 +0530

    Add files via upload

---
+++ 12_list_Concatenate.py
@@ -0,0 +1,8 @@
+# Create a list of student names
+students = ['Ram', 'Sanjay', 'Rahul', 'Ranjeet', 'Rohit']
+print("List of student names",students)
+
+# Concatenate lists
+more_students = ['Harshit', 'Sunil']
+all_students = students + more_students
+print("List after concatenating :", all_students)
