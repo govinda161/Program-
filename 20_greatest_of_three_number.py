e890696a41c5b88a27408b33b20e016a9505def5
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 20:04:33 +0530

    Add files via upload

---
+++ 20_greatest_of_three_number.py
@@ -0,0 +1,9 @@
+a=1
+b=2
+c=3
+if a>=b and a>=c:
+    print("A is greatest amongs other values",a)
+elif b>=a and b>=c:
+    print("B is greatest among other values",b)
+else:
+    print("C is greatest among other values",c)
