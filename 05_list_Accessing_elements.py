5e84bbb3cd7b07c1fff9dd56ca99fa3c523d7fc2
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 15:19:39 +0530

    Add files via upload

---
+++ 05_list_Accessing_elements.py
@@ -0,0 +1,7 @@
+# Create a list of student names
+students = ['Ram', 'Sanjay', 'Rahul', 'Ranjeet', 'Rohit']
+
+# Accessing elements
+print("List of students:", students)
+print("First student:", students[0])
+print("Last student:", students[-1])
