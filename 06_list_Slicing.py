67ed42112bb6ab2c4cf99f3ed3cfc1cb93a5aede
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 15:47:43 +0530

    06_list_Slicing.py

--- 06_list_Slicing.py
+++ 06_list_Slicing.py
@@ -1,5 +1,6 @@
 # Create a list of student names
 students = ['Ram', 'Sanjay', 'Rahul', 'Ranjeet', 'Rohit']
+print("List of student names",students)
 
 # Slicing
 print("First three students:", students[:3])
