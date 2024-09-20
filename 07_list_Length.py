25b4982f725b8dda2506d5ea16d037b5eedae471
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 15:47:12 +0530

    07_list_Length.py

--- 07_list_Length.py
+++ 07_list_Length.py
@@ -1,5 +1,6 @@
 # Create a list of student names
 students = ['Ram', 'Sanjay', 'Rahul', 'Ranjeet', 'Rohit']
+print("List of student names",students)
 
 # Length of the list
 print("Number of students:", len(students))
