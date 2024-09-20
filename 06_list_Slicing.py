eb0eec78980479e7b56a3804aef940cb60e93555
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 15:23:04 +0530

    Add files via upload

---
+++ 06_list_Slicing.py
@@ -0,0 +1,6 @@
+# Create a list of student names
+students = ['Ram', 'Sanjay', 'Rahul', 'Ranjeet', 'Rohit']
+
+# Slicing
+print("First three students:", students[:3])
+print("Last three students:", students[-3:])
