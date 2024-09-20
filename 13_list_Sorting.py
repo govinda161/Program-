15ee295dd2b861ed11c253b99a92da136ca63f05
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 16:05:00 +0530

    Add files via upload

---
+++ 13_list_Sorting.py
@@ -0,0 +1,7 @@
+# Create a list of student names
+students = ['Ram', 'Sanjay', 'Rahul', 'Ranjeet', 'Rohit', 'Harshit', 'Sunil']
+print("List of student names",students)
+
+# Sorting (alphabetical order)
+students.sort()
+print("List after sorting :", students)
