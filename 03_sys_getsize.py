f8c7382f4fe76e0bb3145fd6e87f1d10afaa9442
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 14:58:35 +0530

    Add files via upload

---
+++ 03_sys_getsize.py
@@ -0,0 +1,9 @@
+age=20
+import sys
+print(f'The max capacity of (integer) is {sys.getsizeof(age)}')
+
+name = 'Govinda'
+print(f'The capacity of text (string) is {sys.getsizeof(name)}')
+
+height=165.5
+print(f'The capacity of text (boolean) is {sys.getsizeof(height)}')
