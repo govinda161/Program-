ac9cb4693472aabd032543f628c0d8f5e376e835
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 16:05:16 +0530

    Add files via upload

---
+++ 14_list_Reverse_sorting.py
@@ -0,0 +1,7 @@
+# Create a list of student names
+students = ['Ram', 'Sanjay', 'Rahul', 'Ranjeet', 'Rohit', 'Harshit', 'Sunil']
+print("List of student names",students)
+
+# Reverse sorting
+students.sort(reverse=True)
+print("List after sorting reverse :", students)
