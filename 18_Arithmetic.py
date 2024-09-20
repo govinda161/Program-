adcf9360433466b90233fd1286d290981b62fb47
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 19:52:43 +0530

    Add files via upload

---
+++ 18_Arithmetic.py
@@ -0,0 +1,28 @@
+# Assignment operators example program
+
+x = 10
+    
+    # Using assignment operators
+x += 5  # x = x + 5
+print("x after addition:",x)
+    
+x -= 3  #  x = x - 3
+print("x after subtraction:",x)
+    
+x *= 2  # x = x * 2
+print("x after multiplication:",x)
+    
+x /= 4  # x = x / 4
+print("x after division:",x)
+    
+
+    
+x **= 2  # x = x ** 2
+print("x after exponentiation:",x)
+
+x //= 2  # x = x // 2
+print("x after floor:",x)
+
+
+x %= 3  #  x = x % 3
+print("x after modulus:",x)
