854b5c873e70f97999aca0bb86a73d2f16d3e11c
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 15:39:19 +0530

    Add files via upload

---
+++ 11_list_Pop.py
@@ -0,0 +1,7 @@
+# Create a list of student names
+students = ['Ram', 'Sanjay', 'Rahul', 'Ranjeet', 'Rohit']
+print("List of student names",students)
+
+# Pop
+removed_student = students.pop(4)
+print(f"List after popping last element ({removed_student}):", students)
