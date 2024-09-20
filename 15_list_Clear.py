1cbd793e28270951ccb55f7303dbc80bae25220b
govinda161 <chauhangovinda161@gmail.com>
2024-09-20 16:08:22 +0530

    Add files via upload

---
+++ 15_list_Clear.py
@@ -0,0 +1,7 @@
+# Create a list of student names
+students = ['Ram', 'Sanjay', 'Rahul', 'Ranjeet', 'Rohit', 'Harshit', 'Sunil']
+print("List of student names",students)
+
+# 11. Clear the list
+students.clear()
+print("List after clearing all elements:", students)
