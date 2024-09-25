9b7bbd48c52c374a111730b91bfdd851bdeb72ea
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-25 13:46:21 +0530

    04_Real_Img_No..py

--- 04_Real_Img_No..py
+++ 04_Real_Img_No..py
@@ -1,5 +1,7 @@
+# Creating complex numbers
 x=5
 y=7
+# Performing operations
 z=complex(x,y)
 print('The real part of complex number is:', z.real)
 print('The imaginary part of complex number is:', z.imag)
