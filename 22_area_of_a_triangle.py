62a8ad647b4f9f9910ac4e3a8bb0a48982636abe
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-25 14:26:44 +0530

    22_area_of_a_triangle.py

--- 22_area_of_a_triangle.py
+++ 22_area_of_a_triangle.py
@@ -1,7 +1,10 @@
-#b=base
-#h=height
-
+# b=base
+# h=height
+# Enter your base length
 b = float(input("Enter the base length of the triangle: "))
+# Enter your height
 h = float(input("Enter the height of the triangle: "))
+# Calculate the area
 a = 0.5 * b * h
+# Print the result.
 print("The area of the triangle is:",a)
