7eea37bd7cff6392c5e5de35b97f0795883303e8
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-25 14:31:28 +0530

    23_area_of_a_rectangle.py

--- 23_area_of_a_rectangle.py
+++ 23_area_of_a_rectangle.py
@@ -1,9 +1,13 @@
+# inputs 
 length = 5
 width = 3
+# Calculate the area
 area = length * width
+# Print the result
 print("The area of the rectangle is :",area)
-
-
+#input
 side = 4
+# Calculate the area
 area = side ** 2
+# Print the result 
 print(f"The area of the square ",area)
