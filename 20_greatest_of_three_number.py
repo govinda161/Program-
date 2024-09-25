6524eab753f2146d1d9affb8d0187c7d118951df
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-25 14:16:44 +0530

    20_greatest_of_three_number.py

--- 20_greatest_of_three_number.py
+++ 20_greatest_of_three_number.py
@@ -1,9 +1,10 @@
+# Find the three greatest numbers
 a=1
 b=2
 c=3
-if a>=b and a>=c:
+if a>=b and a>=c:    # If a is greater than b and a is greater than c, then a is greatest among other values
     print("A is greatest amongs other values",a)
-elif b>=a and b>=c:
+elif b>=a and b>=c:    # If b is greater than a and b is greater than c, then b is greatest among other values
     print("B is greatest among other values",b)
-else:
+else:    # then c is greatest among other values
     print("C is greatest among other values",c)
