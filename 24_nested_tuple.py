c8b2e8f6f6e74d8295ca5abc03015f86288728e3
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-25 15:33:10 +0530

    Add files via upload

---
+++ 24_nested_tuple.py
@@ -0,0 +1,9 @@
+# Code for creating nested tuples
+tuple1 = (0, 1, 2, 3)
+tuple2 = ('Python', 'Data Analyst')
+
+tuple3 = (tuple1, tuple2)
+# Print the result
+print(tuple1)
+print(tuple2)
+print("After nested tuple :", tuple3)
