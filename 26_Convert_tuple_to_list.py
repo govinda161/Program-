30b5b326d8d5d05d6ebc1737b52abcea041f468f
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-25 15:46:33 +0530

    Add files via upload

---
+++ 26_Convert_tuple_to_list.py
@@ -0,0 +1,7 @@
+# Define a tuple
+tuples = (1, 2, 3, 4, 5, 6, 7)
+print("The original tuples :",tuples)
+
+# Convert the tuple to a list
+lists = list(tuples)
+print("After convert the tuple to list :",lists)
