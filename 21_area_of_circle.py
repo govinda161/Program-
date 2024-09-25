52bd515a434e8b90fda1bb4319e409fef63dad57
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-25 14:23:07 +0530

    21_area_of_circle.py

--- 21_area_of_circle.py
+++ 21_area_of_circle.py
@@ -1,6 +1,8 @@
-#area
-#radius
+# Enter your radius
 r = float(input("Enter the radius of the circle: "))
+# Define the value of Pi
 pi=3.14
+# Calculate the area
 a = pi*r** 2
+# Print the result
 print("The area of circle is: ",a)
