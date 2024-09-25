f16a48ed46f2849c7408d6c93d52e760690ee34e
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-25 15:37:41 +0530

    Add files via upload

---
+++ 25_Concatenate_tuple.py
@@ -0,0 +1,17 @@
+# Concatenating tuples to nested tuples
+# using + operator + ", " operator during initialization
+
+# initialize tuples
+test_tup1 = (3, 4)
+test_tup2 = (5, 6)
+
+# printing original tuples
+print("The original tuple 1 : " ,test_tup1)
+print("The original tuple 2 : " ,test_tup2)
+
+# Concatenating tuples to nested tuples
+# using + operator
+res = test_tup1 + test_tup2
+
+# printing result
+print("Tuples after Concatenating : " ,res)
