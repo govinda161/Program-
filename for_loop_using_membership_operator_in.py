3a94744ccf6d60f24162463f92d7c0c537f7bac2
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-26 13:36:04 +0530

    Add files via upload

---
+++ for_loop_using_membership_operator_in.py
@@ -0,0 +1,16 @@
+a=['Hello Everyone','How are you','Fine']
+for b in a:
+    print(b)
+
+a=['Hello Everyone','How are you','Fine']
+for b in a:
+    print("Length is :",len(b))
+    
+c=input('Enter your no. :')
+for n in c:
+      print(n*2)
+               
+for list in range(0,10,4):
+    print('Division :',list/2)
+
+
