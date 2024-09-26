0e4d7624465b10f3df57e4b82c3b7aaa301c458f
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-26 12:51:59 +0530

    Add files via upload

---
+++ comparison_operators.py
@@ -0,0 +1,21 @@
+# Sample values
+a = 10
+b = 20
+
+# Equal to
+print("Equal to ( == ) :", a == b)  # Output: False
+
+# Not equal to
+print("Not equal to ( != ) :", a != b)  # Output: True
+
+# Greater than
+print("Greater than ( > ) :", a > b)   # Output: False
+
+# Less than
+print("Less than ( < ) :", a < b)  # Output: True
+
+# Greater than or equal to
+print("Greater than or equal to ( >= ) :", a >= b)  # Output: False
+
+# Less than or equal to
+print("Less than or equal to ( <= b ) :", a <= b) # Output: True
