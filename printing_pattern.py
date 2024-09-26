22b0acc7a8664ee2607aaa0d6b8b07add4e5fd58
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-09-26 13:52:09 +0530

    Add files via upload

---
+++ printing_pattern.py
@@ -0,0 +1,8 @@
+# Right-Angled Triangle Pattern
+print("Right-Angled Triangle Pattern\n")
+for x in range(1,6):     # Number of rows
+    for y in range(1,x+1):   # Loop through each row
+        print("*",end='')   # Print '*' repeated 'x' times
+    print("")
+    
+
