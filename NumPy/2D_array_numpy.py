620bb3f34fdfa6f23db97a33e00bce52baefc9e7
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-10-01 14:23:05 +0530

    Add files via upload

---
+++ NumPy/2D_array_numpy.py
@@ -0,0 +1,10 @@
+'''how do you create a 2D array (matrix) in numpy?'''
+# Import the NumPy library
+import numpy as np
+
+# Create a two-dimensional NumPy array (matrix)
+arr = np.array([[1,2],   # First row of the matrix
+
+                [3,4]])  # Second row of the matrix
+# Print result
+print("2D Array (Matrix) :",arr)
