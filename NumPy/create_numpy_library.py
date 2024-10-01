1f2a0f7e158a17f03a672549cebd4a438d5dbbf2
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-10-01 14:19:20 +0530

    Add files via upload

---
+++ NumPy/create_numpy_library.py
@@ -0,0 +1,9 @@
+'''how do you create a numpy array?'''
+# Import the NumPy library
+import numpy as np
+
+# Create a one-dimensional NumPy array
+arr = np.array([1,2,3,4,5,6])
+
+# Print the arrays
+print("1D array :",arr) # Display the one-dimensional array
