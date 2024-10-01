f60f199bfe500ebde5515871996c6f4603fe8db2
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-10-01 14:25:27 +0530

    Add files via upload

---
+++ NumPy/data_type_numpy.py
@@ -0,0 +1,10 @@
+''' how do you get the data type of a numpy array? '''
+# Import the NumPy library
+import numpy as np
+
+# Create a NumPy array with specific data
+arr = np.array([1,2,3])  # Create a one-dimensional array
+
+# Print result
+print("Type of array :",arr.dtype) # Display the data type of the array
+
