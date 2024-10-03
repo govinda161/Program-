184d5d07dfd890914aafa0878714a2233f75e1d2
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-10-03 13:29:33 +0530

    Add files via upload

---
+++ NumPy/slice_numpy_array.py
@@ -0,0 +1,10 @@
+''' how do you slice a numpy array? '''
+# Import the NumPy library
+import numpy as np
+
+# Create NumPy arrays
+arr = np.array([1,2,3,4,5])
+
+# Print result
+print("First element :",arr[0])
+print("Last element :",arr[::-1])
