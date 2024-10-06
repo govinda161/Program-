dedb0899bb6039c77581f519b8de7131c52403eb
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-10-06 23:11:36 +0530

    Add files via upload

---
+++ NumPy/two_product_categories.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Revenue for each category
+category1_revenue = np.array([500, 600, 700, 550])
+category2_revenue = np.array([450, 700, 800, 600])
+
+# Calculate total revenue
+total_revenue = category1_revenue + category2_revenue
+
+print("Total Revenue:", total_revenue)
