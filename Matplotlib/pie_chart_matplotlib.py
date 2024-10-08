69d2a383ef97de0812562ae86a42772a8ac28fc3
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-10-08 12:23:28 +0530

    Add files via upload

---
+++ Matplotlib/pie_chart_matplotlib.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+# Market share data
+manufactures = ['Samsung', 'Apple', 'Realme', 'Vivo', 'Others']
+market_share = [20, 30, 25, 18, 15]
+# Create a pie chart
+colors = ['blue', 'yellow', 'green', 'pink', 'grey']
+# Create a pie chart
+plt.pie(market_share, labels=manufactures, colors=colors, autopct ='%.2f')
+# Title
+plt.title('Smartphone Market Share')
+# Show the pie chart
+plt.show()
