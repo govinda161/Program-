167791795290c2707a9dd0fb0cc0d7365e3ad07a
Govinda Chauhan <chauhangovinda161@gmail.com>
2024-10-22 14:04:11 +0530

    Add files via upload

---
+++ first_10 _natural _numbers.py
@@ -0,0 +1,7 @@
+# Set the number of natural numbers to print
+n = 10  # We want to print the first 10 natural numbers
+
+# Loop through numbers from 1 to n (inclusive)
+for i in range(1, n + 1):  # range(1, 11) generates numbers from 1 to 10
+    # Print the current natural number
+    print(i)  # Output the current number in the iteration
